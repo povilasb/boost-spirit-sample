3091b9a283e2daac579987eba0520e2828f94b51
Povilas Balciunas <balciunas90@gmail.com>
2014-07-03 14:15:41 +0300

    Adds more sophisticated parsing samples.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 
 #include <boost/spirit/include/qi.hpp>
 
+// This include is crucial to write semantix actions in such form:
+// qi::int_[qi::_val += qi::_1]
+#include <boost/spirit/include/phoenix_operator.hpp>
+
+#include <boost/fusion/include/adapt_struct.hpp>
+
 
 namespace qi = boost::spirit::qi;
 
@@ -35,7 +41,29 @@ void parse_number_semantic_action(const string& str,
  */
 int sum_csv_numbers(const string& str);
 
+/**
+ * Sums comma separated numbers in alternative way - using Boost.Phoenix
+ * for semantic actions.
+ *
+ * @param str comma separated number list string.
+ * @return number sum.
+ */
+int sum_csv_numbers_alt(const string& str);
+
+/**
+ * Runs number parsing samples.
+ */
 void run_basic_samples(void);
+
+/**
+ * Parses simple file matching pattern. E.g. hidden file name matching pattern
+ * could be "home/user/.*". Parser tokenizes file/directory names and
+ * expansions symbol '*'. Then you can easily traverse std::vector trough
+ * path items 'home', 'user', '.' and check if the yalso have expansion symbol.
+ *
+ * @param pattern pattern string to parse.
+ */
+void parse_file_match_pattern(const string& pattern);
 
 
 int
@@ -47,7 +75,12 @@ main(void)
 	});
 */
 
-	cout << "CSV number sum: " << sum_csv_numbers("1,2,3") << '\n';
+	//cout << "CSV number sum: " << sum_csv_numbers("1,2,3") << '\n';
+	//cout << "CSV number sum: " << sum_csv_numbers_alt("1,2,3") << '\n';
+
+	parse_file_match_pattern("part*");
+	parse_file_match_pattern("home");
+	parse_file_match_pattern("home/povilas/prefix*");
 
 	return 0;
 }
@@ -109,4 +142,65 @@ sum_csv_numbers(const string& str)
 	}
 
 	return result;
+}
+
+
+int
+sum_csv_numbers_alt(const string& str)
+{
+	qi::rule<string::const_iterator, int()> number_rule(
+		qi::eps[qi::_val = 0] >>
+		qi::int_[qi::_val += qi::_1] >> *(','
+			>> qi::int_[qi::_val += qi::_1]));
+
+	int result = -1;
+	bool match = qi::parse(str.begin(), str.end(), number_rule, result);
+	if (!match) {
+		cout << "Int parse failed." << '\n';
+	}
+
+	return result;
+}
+
+
+struct file_pattern {
+	string file_part;
+	bool expansion;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(
+	file_pattern,
+	(string, file_part)
+	(bool, expansion)
+)
+
+void
+parse_file_match_pattern(const string& pattern)
+{
+	qi::rule<string::const_iterator, char()> expansion(qi::char_('*'));
+	qi::rule<string::const_iterator, char()> separator(qi::char_('/'));
+	// All characters except special symbols '*' and '/'.
+	qi::rule<string::const_iterator, string()> file_name(
+		+(qi::char_ - (expansion | separator)));
+
+	qi::rule<string::const_iterator, struct file_pattern()> path_part(
+		qi::hold[file_name >> expansion]
+		| (file_name >> qi::attr(false)));
+
+	qi::rule<string::const_iterator, std::vector<struct file_pattern>()>
+		path(path_part % separator);
+
+	//struct file_pattern result{"", '\0'};
+	std::vector<struct file_pattern> result;
+	bool match = qi::parse(pattern.begin(), pattern.end(), path,
+		result);
+	if (!match) {
+		cout << "Bad file pattern." << '\n';
+	}
+
+	for (auto fpatt : result) {
+	cout << "File pattern: " << fpatt.file_part << " " << "; expansion: "
+		<< fpatt.expansion << '\n';
+	}
+	cout << '\n';
 }
